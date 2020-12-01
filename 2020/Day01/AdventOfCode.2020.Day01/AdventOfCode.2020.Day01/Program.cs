9d5930fd9d2462f85b0f53455ed00a741bbdad41
Kris Hatcher <kris.hatcher@gmail.com>
2020-12-01 11:52:18 -0500

    2020.01.1 - Complete

---
+++ 2020/Day01/AdventOfCode.2020.Day01/AdventOfCode.2020.Day01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace AdventOfCode._2020.Day01
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var file = File.OpenText(args[0]);
+            var jsonString = file.ReadToEnd();
+            var expRptList = JsonConvert.DeserializeObject<List<int>>(jsonString);
+            var finalResult = 0;
+
+            foreach (var expense in expRptList)
+            {
+                foreach (var expRptItem in expRptList.Where(expRptItem => expRptItem != expense).Where(expRptItem => expRptItem + expense == 2020))
+                {
+                    finalResult = expRptItem * expense;
+                    break;
+                }
+
+                if (finalResult != 0) break;
+            }
+
+            Console.WriteLine("Result: " + finalResult);
+            file.Dispose();
+        }
+    }
+}
